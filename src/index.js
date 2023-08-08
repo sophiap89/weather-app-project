e4a9b64b0da0f5c52276af516a522e3bf7567be8
sophiap89 <139080858+sophiap89@users.noreply.github.com>
2023-08-08 12:56:11 +0100

    Updated html and js to reflect most current version of app from codesandbox

--- src/index.js
+++ src/index.js
@@ -15,14 +15,38 @@ let currentDay = `${day} ${hour}:${minutes}`;
 let h5 = document.querySelector("h5");
 h5.innerHTML = `${currentDay}`;
 
-// Challenge 2 //
-
-function searchCity(event) {
+function form(event) {
   event.preventDefault();
   let input = document.querySelector("#search-engine");
-  let currentPlace = `${input.value}`;
-  let newCity = document.querySelector("#city-weather");
-  newCity.innerHTML = currentPlace;
+  let apiKey = "bd3bb6534458ba51b48c49f5155745b6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}&units=metric`;
+
+  axios.get(`${apiUrl}`).then(showTemp);
 }
-let form = document.querySelector("form");
-form.addEventListener("submit", searchCity);
+let click = document.querySelector("form");
+click.addEventListener("submit", form);
+
+function showTemp(response) {
+  let temp = Math.round(response.data.main.temp);
+  let tempElement = document.querySelector("#temp-display");
+  tempElement.innerHTML = `${temp}`;
+  let newCity = document.querySelector("#city-weather");
+  newCity.innerHTML = response.data.name;
+}
+
+function currentLocation(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "bd3bb6534458ba51b48c49f5155745b6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(`${apiUrl}`).then(showTemp);
+}
+
+function locationButton() {
+  navigator.geolocation.getCurrentPosition(currentLocation);
+}
+navigator.geolocation.getCurrentPosition(currentLocation);
+
+let submit = document.querySelector("#current-location-button");
+submit.addEventListener("click", locationButton);
